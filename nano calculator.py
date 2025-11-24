0a567c081d5a97f50821223379c9ce3dc8bf9bf5
madesh-R6 <tamilananime16@gmail.com>
2025-11-24 12:05:06 +0530

    day 3 program

---
+++ nano calculator.py
@@ -0,0 +1,8 @@
+
+def add(a,b):
+    return a+b
+
+a=int(input("enter a first number:"))
+b=int(input("enter a second number:"))
+
+print("result:",add(a,b))
---
+++ nano greet.py
@@ -0,0 +1,5 @@
+def greet(name):
+    print("hello!",name)
+
+user_name=input("enter your name:")
+greet(user_name)
